04b76344ba1be1308c913f583db087043908f7e0
Pablo Jean Rozario <pablo.jean.eel@gmail.com>
2023-10-29 12:00:47 -0300

    :minidisc: configured cubemx, added submodules

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define W25_CS_Pin GPIO_PIN_4
+#define W25_CS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
